dab9959d8df75ed5a4bf7a1626a3a47886945bcc
Reyzim10 <francisaraujo0244@gmail.com>
2025-01-31 16:24:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function insertionSort(){
+    let atual;
+    let vetor = [1, 5, 2, 7, 4, 3, 8]
+        
+    for (let i = 1; i < vetor.length; i++){
+        let j = i - 1;
+        atual = vetor[i];
+        while(j >= 0 && atual < vetor[j]){
+            vetor[j+1] = vetor[j];
+            j--;
+        }
+        vetor[j+1] = atual;
+    }
+    return vetor;  
+}
+
+console.log(insertionSort());
